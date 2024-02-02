4a8e88c12e5c44dffe1cb460de845eae81363df7
Dr Maxim Orlovsky <orlovsky@ubideco.org>
2024-02-02 15:43:50 +0100

    refactor named/unnamed attributes

--- src/t_expr.rs
+++ src/t_expr.rs
@@ -19,30 +19,44 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use std::fmt;
 use std::fmt::{Display, Formatter};
 
 use amplify::confinement::{SmallVec, TinyVec};
 use strict_encoding::Ident;
 
 pub trait Predicate: Clone + Eq {
-    type AttrVal: AttributeValue;
+    type Attr: Attribute;
+    type AttrNamed: AttributeNamed;
 }
-pub trait AttributeValue: Clone + Eq + Display {}
+pub trait Attribute: Clone + Eq {
+    fn value(&self) -> Ident;
+}
 
-#[derive(Clone, Eq, PartialEq, Hash, Debug, Display)]
-pub enum Attr<A: AttributeValue> {
-    #[display(inner)]
-    Unnamed(A),
+pub trait AttributeNamed: Attribute {
+    fn name(&self) -> Ident;
+}
 
-    #[display("{0}={1}")]
-    Named(Ident, A),
+#[derive(Clone, Eq, PartialEq, Hash, Debug)]
+pub enum Attr<U: Attribute, N: AttributeNamed> {
+    Unnamed(U),
+    Named(N),
+}
+
+impl<U: Attribute, N: AttributeNamed> Display for Attr<U, N> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match self {
+            Attr::Unnamed(a) => write!(f, "{}", a.value()),
+            Attr::Named(a) => write!(f, "{}~{}", a.name(), a.value()),
+        }
+    }
 }
 
 #[derive(Clone, Eq, PartialEq)]
 pub struct TExpr<P: Predicate> {
     pub subject: Ident,
     pub predicate: P,
-    pub attributes: SmallVec<Attr<P::AttrVal>>,
+    pub attributes: SmallVec<Attr<P::Attr, P::AttrNamed>>,
     pub content: TinyVec<Box<TExpr<P>>>,
 }
 
